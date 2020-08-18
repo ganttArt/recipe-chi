d552cb1efdc95a4000fa276469eaf18d048b1094
Chris Gantt <gantt.art@gmail.com>
2020-08-18 16:24:22 -0700

    Created admin and db models. Admin succesfully performs CRUD functions

--- groceries_app/admin.py
+++ groceries_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Meal, Ingredient, Measurement, IngredientQuantity
 
-# Register your models here.
+admin.site.register(Meal)
+admin.site.register(Ingredient)
+admin.site.register(Measurement)
+admin.site.register(IngredientQuantity)
--- groceries_app/models.py
+++ groceries_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Meal(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+    times_eaten = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    ingredient = models.CharField(max_length=60, unique=True)
+
+    def __str__(self):
+        return self.ingredient
+
+
+class Measurement(models.Model):
+    measurement = models.CharField(max_length=20, unique=True)
+    
+    def __str__(self):
+        return self.measurement
+
+
+class IngredientQuantity(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+    quantity = models.IntegerField()
+    measurement = models.ForeignKey(Measurement, on_delete=models.PROTECT)
+    meal_name = models.ForeignKey(Meal, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.ingredient} for {self.meal_name}'
