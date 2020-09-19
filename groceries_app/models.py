9a63cef70cef769f06637de499d2efc153fa71d4
Chris Gantt <gantt.art@gmail.com>
2020-09-18 15:20:45 -1000

    Models updated to include more Meal information and categories for Ingredients.

--- groceries_app/models.py
+++ groceries_app/models.py
@@ -2,7 +2,18 @@ from django.db import models
 
 class Meal(models.Model):
     name = models.CharField(max_length=60, unique=True)
-    times_eaten = models.IntegerField()
+    times_eaten = models.PositiveIntegerField()
+    photo = models.ImageField()
+    cook_time = models.PositiveIntegerField()
+    portion_number = models.PositiveIntegerField()
+    original_recipe_link = models.URLField()
+    other_link = models.URLField(blank=True)
+    original_recipe_creator = models.CharField(max_length=40)
+    original_recipe_name = models.CharField(max_length=60)
+    recipe_category = models.CharField(max_length=40)
+    description = models.TextField()
+    instructions = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
     
     def __str__(self):
         return self.name
@@ -10,6 +21,7 @@ class Meal(models.Model):
 
 class Ingredient(models.Model):
     ingredient = models.CharField(max_length=60, unique=True)
+    category = models.CharField(max_length=20) # potentially add choices later when I get a good understanding of what the choices are
 
     def __str__(self):
         return self.ingredient
@@ -24,7 +36,7 @@ class Measurement(models.Model):
 
 class IngredientQuantity(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField()
     measurement = models.ForeignKey(Measurement, on_delete=models.PROTECT)
     meal_name = models.ForeignKey(Meal, on_delete=models.PROTECT)
 
