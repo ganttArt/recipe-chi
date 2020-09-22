c5c0a94033d9ac425979ea60941921b361f79d32
Chris Gantt <gantt.art@gmail.com>
2020-09-21 15:08:01 -1000

    Initial design of SQL database finalized and filled with data for 1 meal.

--- groceries_app/models.py
+++ groceries_app/models.py
@@ -1,27 +1,38 @@
 from django.db import models
 
 class Meal(models.Model):
+    instructions_boilerplate = "<li></li>"
+
     name = models.CharField(max_length=60, unique=True)
-    times_eaten = models.PositiveIntegerField()
-    photo = models.ImageField()
-    cook_time = models.PositiveIntegerField()
-    portion_number = models.PositiveIntegerField()
-    original_recipe_link = models.URLField()
-    other_link = models.URLField(blank=True)
-    original_recipe_creator = models.CharField(max_length=40)
-    original_recipe_name = models.CharField(max_length=60)
     recipe_category = models.CharField(max_length=40)
-    description = models.TextField()
-    instructions = models.TextField()
+    times_eaten = models.PositiveIntegerField(default=0)
+    cook_time = models.PositiveIntegerField(null=True, blank=True)
+    portion_number = models.PositiveIntegerField(null=True, blank=True)
+    photo = models.ImageField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    instructions = models.TextField(null=True, blank=True) # add default later
     date_added = models.DateField(auto_now_add=True)
+    original_recipe_creator = models.CharField(max_length=40, null=True, blank=True)
+    original_recipe_name = models.CharField(max_length=60, null=True, blank=True)
+    recipe_link_one = models.URLField(null=True, blank=True)
+    recipe_link_two = models.URLField(null=True, blank=True)
+    affiliate_link = models.URLField(null=True, blank=True)
+    other_link = models.URLField(null=True, blank=True)
     
     def __str__(self):
         return self.name
 
 
 class Ingredient(models.Model):
+    specialty_stores = [
+        ('vegan', 'Vegan Specialty'),
+        ('indian', 'Indian Grocery'),
+        ('asian', 'Asian Grocery'),
+        ('ethiopian', 'Ethiopian Grocery'),
+    ]
     ingredient = models.CharField(max_length=60, unique=True)
-    category = models.CharField(max_length=20) # potentially add choices later when I get a good understanding of what the choices are
+    category = models.CharField(max_length=20, null=True, blank=True) # potentially add choices later when I get a good understanding of what the choices are
+    specialty_location = models.CharField(max_length=10, null=True, blank=True, choices=specialty_stores)
 
     def __str__(self):
         return self.ingredient
@@ -36,7 +47,7 @@ class Measurement(models.Model):
 
 class IngredientQuantity(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
-    quantity = models.PositiveIntegerField()
+    quantity = models.FloatField()
     measurement = models.ForeignKey(Measurement, on_delete=models.PROTECT)
     meal_name = models.ForeignKey(Meal, on_delete=models.PROTECT)
 
