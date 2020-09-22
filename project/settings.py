82902f5ae501e3b754dce9cdd992d1a2732ed6c8
Chris Gantt <gantt.art@gmail.com>
2020-09-22 11:28:02 -1000

    Base template created in new templates directory, setting changed to point to this directory. Basic views and urls setup pointing to base template.

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'groceries_app.apps.GroceriesAppConfig'
+    'groceries_app',
+    # 'groceries_app.apps.GroceriesAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
