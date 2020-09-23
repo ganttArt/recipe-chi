9653ec70833235f548d5eb43492c0370b833c612
Chris Gantt <gantt.art@gmail.com>
2020-09-22 17:47:49 -1000

    Set up views and urls mapping to templates for Home, About, Recipes and Grocery Planner pages. Links included in nav bar.

--- groceries_app/urls.py
+++ groceries_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AboutPageView, RecipesPageView, PlannerPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('about', AboutPageView.as_view(), name='about'),
+    path('recipes', RecipesPageView.as_view(), name='recipes'),
+    path('planner', PlannerPageView.as_view(), name='planner')
 ]
--- groceries_app/views.py
+++ groceries_app/views.py
@@ -2,4 +2,13 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = 'groceries_app/_base.html'
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+class RecipesPageView(TemplateView):
+    template_name = 'recipes/recipes.html'
+
+class PlannerPageView(TemplateView):
+    template_name = 'grocery_planner/planner.html'
