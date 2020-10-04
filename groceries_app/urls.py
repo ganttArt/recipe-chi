704157bdaa6a08d05cb50d0383d1fc94f8755d9e
Chris Gantt <gantt.art@gmail.com>
2020-10-04 05:48:57 -1000

    Created views, urls and templates for the Recipes section of the website (recipes-list and recipes-detail).

--- groceries_app/urls.py
+++ groceries_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, AboutView, RecipesView, MealChoiceView
+from .views import HomeView, AboutView, MealChoiceView, RecipeListView, RecipeDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about', AboutView.as_view(), name='about'),
-    path('recipes', RecipesView.as_view(), name='recipes'),
-    path('meal-choice', MealChoiceView.as_view(), name='meal-choice')
+    path('meal-choice', MealChoiceView.as_view(), name='meal-choice'),
+    path('recipes', RecipeListView.as_view()),
+    path('recipes/detail/<int:pk>', RecipeDetailView.as_view(), name="recipe-detail"), 
 ]
--- groceries_app/views.py
+++ groceries_app/views.py
@@ -1,15 +1,16 @@
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from groceries_app.models import Meal
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
+
 class AboutView(TemplateView):
     template_name = 'about.html'
 
-class RecipesView(TemplateView):
-    template_name = 'recipes/recipes.html'
 
 class MealChoiceView(TemplateView):
     template_name = 'grocery_planner/gp1-meal-choice.html'
@@ -18,3 +19,18 @@ class MealChoiceView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['meal_list'] = Meal.objects.order_by('name')
         return context
+
+
+class RecipeListView(ListView):
+    template_name = 'recipes/recipes_list.html'
+    model = Meal
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['meal_list'] = Meal.objects.order_by('name')
+        return context
+
+
+class RecipeDetailView(DetailView):
+    template_name = 'recipes/recipes_detail.html'
+    model = Meal
