6e319d86513bba84d5b190f540470c57ef7dc872
Chris Gantt <gantt.art@gmail.com>
2020-10-09 10:15:38 -1000

    Revised styling for recipes-detail for the addition of the description card. Removed ingredient-optional field from IngredientQuantity model because the previous migration was unsuccesful and that field is not necessary at the moment.

--- groceries_app/views.py
+++ groceries_app/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from groceries_app.models import Meal
+from groceries_app.models import Meal, IngredientQuantity
 
 
 class HomeView(TemplateView):
@@ -34,3 +34,8 @@ class RecipeListView(ListView):
 class RecipeDetailView(DetailView):
     template_name = 'recipes/recipes_detail.html'
     model = Meal
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ingredient_list'] = IngredientQuantity.objects.filter(meal_name=context['meal'])
+        return context
