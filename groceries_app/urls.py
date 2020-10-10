bd1ccd0f4bb8a8cc2456c93ff645f7123bba8d08
Chris Gantt <gantt.art@gmail.com>
2020-10-10 02:22:26 -1000

    Fixed site issues from git merge with master. Updated navbar links to work on any page.

--- groceries_app/urls.py
+++ groceries_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('about', AboutView.as_view(), name='about'),
     path('meal-choice', MealChoiceView.as_view(), name='meal-choice'),
     path('recipes/detail/<int:pk>', RecipeDetailView.as_view(), name="recipe-detail"), 
-    path('recipes', RecipesView.as_view(), name='recipes'),
+    path('recipes', RecipeListView.as_view(), name='recipes'),
     path('planner/meal-choice', MealChoiceView.as_view(), name='meal-choice')
 ]
