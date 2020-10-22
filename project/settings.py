625ba2adcb665551d597f13d5746450b93cc3628
Chris Gantt <gantt.art@gmail.com>
2020-10-22 13:21:49 -0700

    Updated setting for secret key

--- project/settings.py
+++ project/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gr9w440f%yvxtlxxi82jnnsd4nh@k2t1a24a=$jmc3z(ugae8x'
+# SECRET_KEY = 'gr9w440f%yvxtlxxi82jnnsd4nh@k2t1a24a=$jmc3z(ugae8x'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['recipe-chi.herokuapp.com']
 
 
 # Application definition
