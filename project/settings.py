704c4bd40725235c77ae0684c38e67273607b569
Chris Gantt <gantt.art@gmail.com>
2020-10-27 10:16:11 -0700

    Debug mode turned on.

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['recipe-chi.herokuapp.com']
 
