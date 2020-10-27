f9636c73482412afd6aedece8b47393d0873f142
Chris Gantt <gantt.art@gmail.com>
2020-10-27 10:12:30 -0700

    Debug mode turned off.

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = False
 
 ALLOWED_HOSTS = ['recipe-chi.herokuapp.com']
 
