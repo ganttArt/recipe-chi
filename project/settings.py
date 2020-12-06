84e23ede01ad85bea8db8c6ca8f0d719828e3996
Chris Gantt <gantt.art@gmail.com>
2020-12-06 13:13:22 -0800

    Updated settings for debug mode.

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['RECIPECHI_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['recipe-chi.herokuapp.com', 'recipechi.com']
 
