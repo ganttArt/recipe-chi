f1199079e47bcf4bf4647be8a17bded3a87163d5
Chris Gantt <gantt.art@gmail.com>
2020-12-08 08:28:50 -0800

    Cleaned settings folder.

--- project/settings.py
+++ project/settings.py
@@ -16,9 +16,6 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['RECIPECHI_SECRET_KEY']
 
