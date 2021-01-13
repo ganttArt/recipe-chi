e2d25dde599f7b913b05ce78e4d8d8e1512229f5
Chris Gantt <gantt.art@gmail.com>
2021-01-13 12:10:48 -0800

    Added a section for instructionless recipes in recipe-list.

--- groceries_app/views.py
+++ groceries_app/views.py
@@ -175,3 +175,14 @@ class RecipeDetailView(DetailView):
         context['ingredient_list'] = IngredientQuantity.objects.filter(
             meal_name=context['meal'])
         return context
+
+
+class RecipeDetailSimpleView(DetailView):
+    template_name = 'recipes/recipes_detail_simple.html'
+    model = Meal
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ingredient_list'] = IngredientQuantity.objects.filter(
+            meal_name=context['meal'])
+        return context
